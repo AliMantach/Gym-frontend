ca78fe6fdb6a2b023aa518f4a1a295bdc0331176
AliMantach <alimantach11@gmail.com>
2024-06-29 06:04:07 +0300

    adding_subscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from './auth.gard';
+import { SubscriptionComponent } from './subscription/subscription.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'subscription', component: SubscriptionComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
